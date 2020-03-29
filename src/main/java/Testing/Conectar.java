2d776420b11208a9e0d41570e70d71ed1f02f821
David <vjdavid621@gmail.com>
2020-03-29 02:01:06 -0700

    initalizing java repository

---
+++ src/main/java/Testing/Conectar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testing;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author david
+ */
+public class Conectar {
+    Connection conectar = null;
+    
+    public Connection conexion() {
+        try {
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/Tienda?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "david12345");
+            JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return conectar;
+    }
+}
